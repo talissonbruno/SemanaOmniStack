82a258406a47fed69e7744647cbf79435d67b3ce
Talisson Souza <talissonbrsouza@gmail.com>
2020-03-24 14:29:14 -0300

    udpate routes 2

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/ongs', (request, response) => {
-    const data = request.body;
+routes.post('/ongs', async (request, response) => {
+    const { name, email, whatsapp, city, uf } = request.body;
 
-    console.log(data);
+    const id = crypto.randomBytes(4).toString('HEX');
 
-    return response.json();
+    await connection('ongs').insert({
+        id, 
+        name, 
+        email, 
+        whatsapp, 
+        city, 
+        uf,
+    })
+
+    return response.json({ id });
 });
 
 module.exports = routes;
