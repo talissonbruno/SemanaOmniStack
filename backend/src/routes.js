b5f4cd071c8432ab578f68f45086256cfc7a55fd
Talisson Souza <talissonbrsouza@gmail.com>
2020-03-24 23:32:44 -0300

    create controller incidents get

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,6 +7,7 @@ const routes = express.Router();
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
+routes.get('/incidents', IncidentsController.index);
 routes.post('/incidents', IncidentsController.create);
 
 module.exports = routes;
